1e14473d3e93b65ed4fb75d8d8e9c235ed975b0a
Stanislav bc2025-4 <krichi306@gmail.com>
2025-10-16 00:25:24 +0300

    update index.js

--- index.js
+++ index.js
@@ -12,38 +12,50 @@ program
   .requiredOption('-p, --port <port>', 'server port')
   .configureOutput({
     outputError: (str, write) => {
-     if (str.includes('--input'))
-       write('please specify input file\n');
-     else if (str.includes('--host'))
-       write('please specify server host\n');
-     else if (str.includes('--port'))
-       write('please specify server port\n');
-     else
-       write(str);
+      switch(true)
+      {
+        case (str.includes('--input')):
+          write('please specify input file\n')
+          break
+        case (str.includes('--host')): 
+          write('please specify server host\n')
+          break
+        case (str.includes('--port')):
+          write('please specify server port\n')
+          break
+        default:
+          write(str);
+      }
     }
   });
 
 program.parse(process.argv);
 const options = program.opts();
 
-  // ─── HTTP сервер ──────────────────────────────────────
+const checkFile = async (path) => {
+  try {
+    await fs.access(path);
+  } catch {
+    console.error("cannot find input file");
+    process.exit(1);
+  }
+};
+await checkFile(options.input);
+
   const server = http.createServer(async (req, res) => {
     try {
       const parsedUrl = url.parse(req.url, true);
       const query = parsedUrl.query;
 
-      // Читаємо файл рядок за рядком
       const fileData = await fs.readFile(options.input, 'utf-8');
       const lines = fileData.trim().split('\n');
       const passengers = lines.map(line => JSON.parse(line));
 
-      // Фільтруємо по survived
       let filtered = passengers;
       if (query.survived === 'true') {
         filtered = filtered.filter(p => p.Survived === "1" || p.Survived === 1);
       }
 
-      // Генеруємо XML
       const xmlBuilder = new XMLBuilder({ ignoreAttributes: false, format: true });
       const xmlData = {
         passengers: {
